edbcb74123221feea20cb8a65f6b392f34a1ff42
moonkorea00 <moonkorea00@gmail.com>
2022-01-15 18:31:10 +0900

    폴더/파일 이름 변경

---
+++ team5/29-React-Westagram-5-master/src/Router.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import LoginJeeyoon from './pages/29th kim/LoginJeeyoon/LoginJeeyoon';
+import MainJeeyoon from './pages/29th kim/MainJeeyoon/MainJeeyoon';
+
+import JaehyungLogin from './pages/29th-park/LoginJaehyung/LoginJaehyung';
+import JaehyungMain from './pages/29th-park/MainJaehyung/MainJaehyung';
+
+import LoginJeewon from './pages/29th moon/LoginJeewon/LoginJeewon';
+import MainJeewon from './pages/29th moon/MainJeewon/MainJeewon';
+
+import YejiLogin from './pages/29th-jang/LoginYeji/LoginYeji';
+import YejiMain from './pages/29th-jang/MainYeji/MainYeji';
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-jeeyoon" element={<LoginJeeyoon />} />
+        <Route path="/main-jeeyoon" element={<MainJeeyoon />} />
+
+        <Route path="/login-jaehyung" element={<JaehyungLogin />} />
+        <Route path="/main-jaehyung" element={<JaehyungMain />} />
+
+        <Route path="/login-Jeewon" element={<LoginJeewon />} />
+        <Route path="/main-Jeewon" element={<MainJeewon />} />
+
+        <Route path="/login-yeji" element={<YejiLogin />} />
+        <Route path="/main-yeji" element={<YejiMain />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
